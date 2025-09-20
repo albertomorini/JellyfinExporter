a4e318e2e2be81524183e3489788e8a213eaf39c
albertomorini <99morini@gmail.com>
2025-09-20 22:47:40 +0200

    temp

--- PlaylistMusicDownloader.py
+++ PlaylistMusicDownloader.py
@@ -1,8 +1,23 @@
 import requests
 import os
+import json
+
+
+def LoadRegister():
+    try:
+        config_file = open("./register.json",'r')
+        return json.loads(config_file.read())
+    except Exception as e:
+        config_file = open("./register.json","w")
+        config_file.write()
+        return dict()
+
+def StoreRegister():
+    #TODO
+
 
 BASE_URL = "http://10.0.0.3:8096/"
-HEADERS = {'Authorization': 'MediaBrowser Client="Jellyfin%20Web", Device="Firefox", DeviceId="TW96aWxsYS81LjAgKE1hY2ludG9zaDsgSW50ZWwgTWFjIE9TIFggMTAuMTU7IHJ2OjEzNS4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzEzNS4wfDE3NDA1MTgwNTk1NjE1", Version="10.10.6", Token="cd00993a7bfe4b2caccc288aedf170aa"'}
+HEADERS = {'Authorization': 'MediaBrowser Client="Jellyfin Web", Device="Firefox", DeviceId="TW96aWxsYS81LjAgKFgxMTsgTGludXggeDg2XzY0OyBydjoxNDIuMCkgR2Vja28vMjAxMDAxMDEgRmlyZWZveC8xNDIuMHwxNzU2NTQ3NzI3MTQ2", Version="10.10.7", Token="9b2c5c1a83d5448981ab9cfcf89eadf5"'}
 
 FOLDER_DEST = "/home/alby/MusicExported/"
 
@@ -18,7 +33,6 @@ def downloadFile(fileID,playlistName,fileName, createPlaylistFolder=False):
             else:
                 finalPath = FOLDER_DEST + "downloaded"
                 playlistFile = FOLDER_DEST+"/"+playlistName+".m3u"
-                
                 with open(playlistFile,"a") as f: ## append file name to create the playlist
                     f.write("./downloaded/"+fileName+"\n")
 
@@ -34,8 +48,6 @@ def downloadFile(fileID,playlistName,fileName, createPlaylistFolder=False):
                 
         else:
             print("ERROR ON DOWNLOADING THE SONG - fileid: "+fileID)
-
-
 
 
 def getSongsIDFromPlaylist(PlaylistID):
@@ -58,7 +70,12 @@ def getSongMetadata(songID):
     else:
         return null
 
-#### 
+######################## 
+
+
+def justNewMusic():
+    register = LoadRegister()
+    
 
 ## for each playlist will be created a folder with the same name containing the songs
 def main():
@@ -70,17 +87,17 @@ def main():
     playlists["chillin"] = "4638cefbb8c256e05192e51aed0f48ec"
     playlists["deep night"] = "959668ac405340c31c34046129b35ac1"
     playlists["energy"] = "3c87f65b5fe53cf74c36219e8425af59"
-    # playlists["gym"] = "ff8fe24fbc3bd811a9bfccbd3113c7eb"
-    # playlists["runnin"] = "301709956ac5e8a3d051797c2605fb1d"
     playlists["stuff"] = "1d64b9eab05f552af44fdb8c3efe8353"
+    
 
     for p in playlists:
-        print(p)
+        print("Downloading ", p)
         songs_id = getSongsIDFromPlaylist(playlists[p])
         #check if the playlist file exists, if so, delete (to update all the list) -- the media already existing won't be downloaded again
         playlistFile = FOLDER_DEST+"/"+p+".m3u"
         with open(playlistFile,"w") as f: #erease and write everytime the file
             f.write("#"+p+"\n")
+
         for s in songs_id:
             downloadFile(s, p, getSongMetadata(s))
 
